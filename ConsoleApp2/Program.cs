8f3c91707fb5cf948527ed02a5b1bf862dd06049
unknown <filip.tomanek07@email.cz>
2022-11-22 17:50:11 +0100

    first commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Napis cislo:");
+            string num01 = "";
+            num01 = Console.ReadLine();
+
+            string flip = "";
+
+
+
+            for (int i = num01.Length; i > 0; i--)
+            {
+                flip = flip + num01[i - 1];
+            }
+
+            bool res01 = false;
+
+            if (flip == num01)
+            {
+                res01 = true;
+            }
+
+            Console.WriteLine(res01);
+        }
+    }
+}
